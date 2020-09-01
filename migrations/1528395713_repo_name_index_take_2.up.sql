1a6ff9c6f01b0f66a710af072bf5d3fedbde0a99
Beyang Liu <beyang@sourcegraph.com>
2020-09-01 12:18:54 -0700

    remove CONCURRENTLY

--- migrations/1528395713_repo_name_index_take_2.up.sql
+++ migrations/1528395713_repo_name_index_take_2.up.sql
@@ -1,3 +1,3 @@
 BEGIN;
-CREATE INDEX CONCURRENTLY IF NOT EXISTS repo_name_idx ON public.repo USING btree (lower(name::text) COLLATE pg_catalog."C");
+CREATE INDEX IF NOT EXISTS repo_name_idx ON public.repo USING btree (lower(name::text) COLLATE pg_catalog."C");
 COMMIT;
